a443997e16f72be4e1e13562a77043e5ca306e33
Manfred R. Bihy <126871627+Manfred-R-Bihy@users.noreply.github.com>
2023-12-30 16:53:35 +0100

    Update index.js

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -2,10 +2,12 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
+    const data = req.query.data;
     const responseMessage = name
         ? "Hello, " + name + "! My first function!"
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    context.bindings.outputBlob = context.bindings.inputBlob
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
